adba7b5042fd6600228a470c1022ca36a8ac04b2
Imamovic, Haris <haris.imamovic@bawagpsk.com>
2020-11-16 00:19:46 +0100

    feature: Completed the code with in-memory MongoDB

---
+++ account-details/src/main/java/ba/klika/focus/reactive/accounts/accountdetails/accountdetails/database/Account.java
@@ -0,0 +1,33 @@
+package ba.klika.focus.reactive.accounts.accountdetails.accountdetails.database;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+
+    @Id
+    private String id;
+
+    private String iban;
+    private String ownerFirstName;
+    private String ownerLastName;
+    private String ownerAddress;
+
+
+    public Account(String iban, String ownerFirstName, String ownerLastName, String ownerAddress) {
+        this.iban = iban;
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.ownerAddress = ownerAddress;
+    }
+
+}
