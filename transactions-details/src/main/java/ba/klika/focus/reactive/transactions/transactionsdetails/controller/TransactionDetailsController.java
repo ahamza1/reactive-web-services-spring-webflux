f5dcbf76e34e81eb3e80b33c552f3495f500f068
ahamza <almir.hamza4@gmail.com>
2020-11-18 18:58:01 +0100

    Removed unnecessary logging annotation on controller, logging moved to TransactionDetailsService

--- transactions-details/src/main/java/ba/klika/focus/reactive/transactions/transactionsdetails/controller/TransactionDetailsController.java
+++ transactions-details/src/main/java/ba/klika/focus/reactive/transactions/transactionsdetails/controller/TransactionDetailsController.java
@@ -3,13 +3,11 @@ package ba.klika.focus.reactive.transactions.transactionsdetails.controller;
 import ba.klika.focus.reactive.transactions.transactionsdetails.model.TransactionDetails;
 import ba.klika.focus.reactive.transactions.transactionsdetails.service.ITransactionDetailsService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TransactionDetailsController {
